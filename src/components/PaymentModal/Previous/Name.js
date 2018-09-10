3b8063313f1eafe66f620c24217d9e77bc42ac87
Kirill <kirill.potapkin@kodep.ru>
2018-09-10 09:34:42 +0300

    something fix

--- src/components/PaymentModal/Previous/Name.js
+++ src/components/PaymentModal/Previous/Name.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import shortName from '../../../utils/shortName'
 
 const PreviousName = ({name}) => {
   return(
     <span
       className='previous__list--category'
       title={name}
-      >{name}
-      <span style={{overflow: 'visible'}}>:</span>
+      >{name}:
     </span>
   )
 }
--- src/utils/shortName.js
+++
@@ -1,8 +0,0 @@
-const shortName = name => {
-  if (name.length > 22)
-    return name.substr(0, 22) + '...'
-  else
-    return name
-}
-
-export default (shortName)
